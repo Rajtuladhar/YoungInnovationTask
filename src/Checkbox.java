44b8144ff87ddaf4f31e21e7a1a31509c48b8d4d
Raj Tuladhar <tuladharraj09@gmail.com>
2023-10-10 13:46:31 +0545

    checkbox automation completed

---
+++ src/Checkbox.java
@@ -0,0 +1,26 @@
+package Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkbox {
+
+	public static void main(String[] args) throws Throwable{
+		System.setProperty("Chorme","C:\\selenium webdriver\\chorme driver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/");
+		Thread.sleep(2000);
+		driver.get("https://the-internet.herokuapp.com/checkboxes");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='checkbox'][1]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='checkbox'][2]")).click();
+
+		
+		
+
+	}
+
+}
